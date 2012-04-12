c641ee7a8251ee34419eeeb1799e05fb78ede20f
James Anderson <jra798@mst.edu>
2012-04-11 19:08:10 -0500

    added wiimote to repo because https is broken

---
+++ kinect_mapper/build/CMakeFiles/kinect_mapper.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/james/Code/mesh_mapper/kinect_mapper/src/kinect_mapper.cpp" "/home/james/Code/mesh_mapper/kinect_mapper/build/CMakeFiles/kinect_mapper.dir/src/kinect_mapper.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ kinect_mapper/build/CMakeFiles/kinect_mapper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/kinect_mapper.dir/src/kinect_mapper.o"
+  "../bin/kinect_mapper.pdb"
+  "../bin/kinect_mapper"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/kinect_mapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
